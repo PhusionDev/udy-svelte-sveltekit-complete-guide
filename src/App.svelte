e870701c71ff7c6ed621b34a0ef638df4914850c
Jared Hodgkins <jared.hodgkins@gmail.com>
2024-04-09 10:00:49 -0400

    7. Svelte Components Basic Structure

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = 'Svelte';
+  let src = 'http://picsum.photos/200/200';
+  let string = 'Some <strong>string</strong> with html.'
+</script>
+
+<style>
+  h1 {
+    color: red;
+  }
+</style>
+
+<img {src} alt="Some text {name}"/>
+<h1>Hello {name.toUpperCase()}</h1>
+<p>{@html string}</p>
