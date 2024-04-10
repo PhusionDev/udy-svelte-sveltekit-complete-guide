580be00f8fce3478779a38f70a0010d8302c954c
Jared Hodgkins <jared.hodgkins@gmail.com>
2024-04-10 11:36:07 -0400

    22. Updating Arrays & Objects in Svelte

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,10 +1,23 @@
 <script>
+  import { v4 as uuid } from 'uuid';
+
   import Button from './Button.svelte';
   export let todos = [];
 
   let inputText = '';
 
-  function handleAddTodo(event) {}
+  function handleAddTodo(event) {
+    if (!inputText) return;
+
+    todos = [
+      ...todos,
+      {
+        id: uuid(),
+        title: inputText,
+        completed: false,
+      },
+    ];
+  }
 </script>
 
 <div class="todo-list-wrapper">
@@ -21,6 +34,6 @@
     on:submit|preventDefault={handleAddTodo}
   >
     <input bind:value={inputText} />
-    <Button>Add Todo</Button>
+    <Button type="submit" disabled={!inputText}>Add Todo</Button>
   </form>
 </div>
