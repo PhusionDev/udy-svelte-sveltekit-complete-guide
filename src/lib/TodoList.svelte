964f6412c552703b5b28261c78f6dc199a8b7a25
Jared Hodgkins <jared.hodgkins@gmail.com>
2024-04-10 11:15:00 -0400

    21. Binding Input Value to a Variable

--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,10 +1,26 @@
 <script>
+  import Button from './Button.svelte';
   export let todos = [];
+
+  let inputText = '';
+
+  function handleAddTodo(event) {}
 </script>
 
-<ul>
-  {#each todos as { id, title }, index (id)}
-    {@const number = index + 1}
-    <li>{number}- {title}</li>
-  {/each}
-</ul>
+<div class="todo-list-wrapper">
+  <ul>
+    {#each todos as { id, title }, index (id)}
+      {@const number = index + 1}
+      <li>{number}- {title}</li>
+    {/each}
+  </ul>
+  {inputText}
+  <form
+    class="add-todo-form"
+    action=""
+    on:submit|preventDefault={handleAddTodo}
+  >
+    <input bind:value={inputText} />
+    <Button>Add Todo</Button>
+  </form>
+</div>
